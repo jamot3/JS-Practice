3a41d2d091e29a11f830e03c68529f102fdd3625
Joe M <34356804+jamot3@users.noreply.github.com>
2018-03-19 16:03:43 -0400

    Converted to function

--- sumodds.js
+++ sumodds.js
@@ -1,15 +1,17 @@
 // Prints the sum of all odd integers in an array.
 
-array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-let sum_odd = 0 // Sum to ultimately be printed, 0 as a starting value/placeholder.
-let x = 0  // Iterating variable for indexing through the array.
-
-while (x < array.length) {
-    if (array[x] % 2 === 1) {
-        sum_odd+=array[x]
+const sumOdds = (array) => {
+    let sum_odd = 0;
+    let x = 0;
+    while (x < array.length) {
+    if (array[x] % 2 === 1) { //Determines if number is odd or even. 
+        sum_odd+=array[x] //adds all odd to the sum_odd variable.
+        };
+        x+=1;
     };
-    x+=1
-}
-console.log(sum_odd === 25) //Prints 'true' - 1+3+5+7+9 = 25
+    return sum_odd; //when finished indexing through array, the sum is returned. 
+};
+
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(sumOdds(arr)) //Prints 25: (1+3+5+7+9)
 
