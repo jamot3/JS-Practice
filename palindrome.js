d1be2c78717a1a0d32151c8d4e96f6d504619c5a
Joe M <34356804+jamot3@users.noreply.github.com>
2018-03-19 14:12:53 -0400

    Added switch statement: true/false
    Added a switch statement that prints a string reading: (given word) is a palindrome, OR is not a palindrome, depending on the veracity of the 'palindrome' variable.

--- palindrome.js
+++ palindrome.js
@@ -1,9 +1,11 @@
 // Since I just started Code Academy's JS course yesterday, I still haven't gotten to defining methods, so 
 // I'm still just making little snippets of code that print values to the console. This one takes in a word 
-// and returns 'true' or 'false' whether or not it's a palindrome. It utilizes one iterating variable to index
-// the word from the beginning, x, and another to index the word starting from the end, y. Then, x is iterated by
-// +1 and y by -1 until x reaches the end of the string - if any differences are detected, the 'palindrome' variable
-// (which defaults to 'true') changes in value to 'false', indicating the word isn't a palindrome. 
+// and returns 'true' or 'false' whether or not it's a palindrome. 
+//
+// It utilizes one iterating variable to index the word from the beginning, x, and another to index 
+// the word starting from the end, y. Then, x is iterated by +1 and y by -1 until x reaches the end of 
+// the string - if any differences are detected, the 'palindrome' variable (which defaults to 'true') 
+// changes in value to 'false', indicating the word isn't a palindrome. 
 
 let word = "tattarrattat"
 let x = 0 
@@ -17,6 +19,15 @@ while (x < word.length) {
     x+=1
     y-=1
 }
-console.log(palindrome) // PRINTS 'TRUE'
+
+switch(palindrome) {
+    case true:
+        console.log(`${word} is a palindrome.`);
+        break;
+    case false:
+        console.log(`${word} is not a palindrome.`);
+        break;
+}
+//in the case of the example word, 'tattarrattat', our program prints to the console: 'tattarrattat is a palindrome.'
 
        
