0aa94b56a18eb19e2d507553ac7ae0ca46615591
Joe M <34356804+jamot3@users.noreply.github.com>
2018-03-19 16:22:08 -0400

    Converted to function
    Converted this from a simple block of code to a function - I just got to defining functions in CodeAcademy, so I figured it would be prudent to refactor this code into function form.

--- palindrome.js
+++ palindrome.js
@@ -1,33 +1,20 @@
-// Since I just started Code Academy's JS course yesterday, I still haven't gotten to defining methods, so 
-// I'm still just making little snippets of code that print values to the console. This one takes in a word 
-// and returns 'true' or 'false' whether or not it's a palindrome. 
-//
-// It utilizes one iterating variable to index the word from the beginning, x, and another to index 
-// the word starting from the end, y. Then, x is iterated by +1 and y by -1 until x reaches the end of 
-// the string - if any differences are detected, the 'palindrome' variable (which defaults to 'true') 
-// changes in value to 'false', indicating the word isn't a palindrome. 
+//Function takes in a word and returns 'true' or 'false' if the word is a palindrome - if it is the same both forwards and backwards. 
 
-let word = "tattarrattat"
-let x = 0 
-let y = (word.length - 1)
-let palindrome = true
-
-while (x < word.length) { 
-    if (word[x] !== word[y]){
-        palindrome = false
-    }
-    x+=1
-    y-=1
+const isPalindrome = (word) => {
+    let x = 0; //indexes word from front->back
+    let y = (word.length - 1); //indexes word from back->front
+    
+    while (x < word.length) {
+        if (word[x] !== word[y]) { //if x&y aren't equivalent, it means the word isn't the same from back to front - returns 'false'.
+            return false;
+        }
+        x+=1;
+        y-=1;
+        }
+    return true; //if the function hasn't returned an answer before this point, the word is a palindrome - returns 'true'. 
 }
 
-switch(palindrome) {
-    case true:
-        console.log(`${word} is a palindrome.`);
-        break;
-    case false:
-        console.log(`${word} is not a palindrome.`);
-        break;
-}
-//in the case of the example word, 'tattarrattat', our program prints to the console: 'tattarrattat is a palindrome.'
+console.log(isPalindrome("tattarrattat")) // Output === true
+console.log(isPalindrome("gandalf")) //Output === false
 
        
