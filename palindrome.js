9d613d383ca1d7540ea09842482a5c2b1478fcab
Joe M <34356804+jamot3@users.noreply.github.com>
2018-03-19 12:08:13 -0400

    Added description

--- palindrome.js
+++ palindrome.js
@@ -1,3 +1,10 @@
+// Since I just started Code Academy's JS course yesterday, I still haven't gotten to defining methods, so 
+// I'm still just making little snippets of code that print values to the console. This one takes in a word 
+// and returns 'true' or 'false' whether or not it's a palindrome. It utilizes one iterating variable to index
+// the word from the beginning, x, and another to index the word starting from the end, y. Then, x is iterated by
+// +1 and y by -1 until x reaches the end of the string - if any differences are detected, the 'palindrome' variable
+// (which defaults to 'true') changes in value to 'false', indicating the word isn't a palindrome. 
+
 word = "tattarrattat"
 x = 0 
 y = (word.length - 1)
