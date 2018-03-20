154bc229a91e5e82861419cf9cc6f2ccd83891d6
Joe M <34356804+jamot3@users.noreply.github.com>
2018-03-20 13:22:19 -0400

    Updated description

--- sumodds_forloop.js
+++ sumodds_forloop.js
@@ -1,4 +1,4 @@
-// Prints the sum of all odd integers in an array.
+// Prints the sum of all odd integers in an array. A 'for loop' version of the sumodds function I previously uploaded.
 function sumOdd (array) {
     let sum = 0
     for (let x = 0; x < array.length; x++) {
@@ -9,11 +9,8 @@ function sumOdd (array) {
     return sum
 }
 array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-console.log(sumOdd(array))
+console.log(sumOdd(array)) //Output === 25
 
-
- // Sum to ultimately be printed, 0 as a starting value/placeholder.
- // Iterating variable for indexing through the array.
 
 
 
