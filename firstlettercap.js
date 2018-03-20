f65d99169b5d9bd355cf2dd048253682afe7c380
Joe M <34356804+jamot3@users.noreply.github.com>
2018-03-20 11:05:45 -0400

    Added annotations; simplified
    Added some notes and simplified two statements - 
    new_word = new_word.join("");
    new_sentence.push(new_word);
    simplified as...
    new_sentence.push(new_word.join(""));

--- firstlettercap.js
+++ firstlettercap.js
@@ -9,7 +9,7 @@ function capsFirstLetter (sentence) {
     while (x < sentence.length) {
         let current_word = sentence[x];
         let y = 0; //iterates through current word
-        let new_word = [];
+        let new_word = [];//container for letters processed by following while loop.
         
         while (y < current_word.length) {
             let current_letter = current_word[y]
@@ -18,14 +18,12 @@ function capsFirstLetter (sentence) {
                 current_letter = current_letter.toUpperCase()
             }
             
-            new_word.push(current_letter);
+            new_word.push(current_letter);//adds processed letters to new_word container
             y+=1;
         }
-        
-        new_word = new_word.join("");
-        new_sentence.push(new_word);
+        new_sentence.push(new_word.join(""));//joins new_word array letters into word, adds to new_sentence 
         x+=1;
     }
-    return new_sentence.join(" ")
+    return new_sentence.join(" ")//joins words in new_sentence by a space, returns finished sentence. 
 }
 console.log(capsFirstLetter("hello my name is joe")) //Output: 'Hello My Name Is Joe'
