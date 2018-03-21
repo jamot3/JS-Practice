2e55ea8a7cf209064b4392db8ae7ec784cafb9aa
Joe M <34356804+jamot3@users.noreply.github.com>
2018-03-21 11:40:06 -0400

    Added notes.

--- gcf.js
+++ gcf.js
@@ -1,10 +1,12 @@
+//Finds the greatest common factor (GCF) of two given numbers. 
+
 function gcf (num1, num2) { 
-    let gcf = null;
-    let x = 0; 
-    while (x <= num1) {
+    let gcf = null; //placeholder value for gcf
+    let x = 0; //iterating variable
+    while (x <= num1) { 
         
-        if (num1%x===0 && num2%x===0) {
-            gcf = x;
+        if (num1%x===0 && num2%x===0) { //if x divides evenly into both, it is a factor. Last factor before termination of loop
+            gcf = x;                    //must be greatest common factor.
         }
         x+=1;
     }
